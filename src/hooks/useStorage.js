c80b8fdbbe009eaa0ff40a2eaa3a760d03d12751
samuelkarlosdev <skdevelopment21@gmail.com>
2025-02-08 23:43:34 -0300

    first commit

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,60 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const useStorage = () => {
+
+    // Busca os itens salvos
+    const getItem = async (key) => {
+        try{
+            const passwords = await AsyncStorage.getItem(key);
+            return JSON.parse(passwords) || [];
+
+        }catch(error){
+            console.log("Error ao buscar", error)
+            return [];
+        }
+    }
+
+    // Salvar um item no storage
+    const saveItem = async (key, value) => {
+        try{
+            let passwords = await getItem(key);
+            
+            passwords.push(value)
+
+            await AsyncStorage.setItem(key, JSON.stringify(passwords))
+
+        }catch(error){
+            console.log("Error ao salvar", error)
+            
+        }
+
+    }
+
+    // Remover algo do storage
+    const removeItem = async (key, item) => {
+        try{
+            let passwords = await getItem(key);
+            
+            let myPasswords = passwords.filter( (password) => {
+                return (password !== item)
+            })
+
+            await AsyncStorage.setItem(key, JSON.stringify(myPasswords))
+            
+            return myPasswords;
+
+        }catch(error){
+            console.log("Error ao deletar", error)
+            
+        }
+    }
+
+    return {
+        getItem,
+        saveItem, 
+        removeItem,
+    }
+
+}
+
+export default useStorage;
